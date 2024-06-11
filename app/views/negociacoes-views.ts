869dd417fa8635b4554f568fd8ce6e3595e4df48
Demiletp <141511721+Demiletp@users.noreply.github.com>
2024-06-11 18:46:23 -0300

    11/06¹

--- app/views/negociacoes-views.ts
+++ app/views/negociacoes-views.ts
@@ -1,10 +1,9 @@
-import { Negociacoes } from "../models/negociacoes.js";
 
-export class NegociacaoView {
-    private elemento: HTMLElement;
-    constructor(seletor: string) {
-        this.elemento = document.querySelector(seletor);
-    }
+import { Negociacoes } from "../models/negociacoes.js";
+import { View } from "./view.js";
+
+export class NegociacaoView extends View {
+
     template(model: Negociacoes): string {
         return `
         <table class="table table-hover table-bordered">
