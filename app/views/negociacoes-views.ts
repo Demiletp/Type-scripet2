68b74376ee46ec02e226986de803e9004aa6b8fa
Demiletp <141511721+Demiletp@users.noreply.github.com>
2024-06-11 19:34:18 -0300

    11/06²

--- app/views/negociacoes-views.ts
+++ app/views/negociacoes-views.ts
@@ -2,7 +2,7 @@
 import { Negociacoes } from "../models/negociacoes.js";
 import { View } from "./view.js";
 
-export class NegociacaoView extends View {
+export class NegociacaoView extends View <Negociacoes> {
 
     template(model: Negociacoes): string {
         return `
