8de674c6e3de20e40c9f0d8cd9b80747f36c7d25
Demiletp <141511721+Demiletp@users.noreply.github.com>
2024-06-10 21:56:03 -0300

    10/06¬

--- dist/js/controllers/negociacao-controller.js
+++ dist/js/controllers/negociacao-controller.js
@@ -1,10 +1,12 @@
 import { Negociacao } from '../models/negociacao.js';
 import { Negociacoes } from '../models/negociacoes.js';
+import { MensagemView } from '../views/mesagem-veiw.js';
 import { NegociacaoView } from '../views/negociacoes-views.js';
 export class NegociacaoController {
     constructor() {
         this.negociacoes = new Negociacoes();
         this.negociacoesView = new NegociacaoView('#negociacaoView');
+        this.mensagemView = new MensagemView('#mensagemView');
         this.inputData = document.querySelector('#data');
         this.inputQuantidade = document.querySelector('#quantidade');
         this.inputValor = document.querySelector('#valor');
@@ -14,6 +16,7 @@ export class NegociacaoController {
         const negociacao = this.criaNegociacao();
         this.negociacoes.adiciona(negociacao);
         this.negociacoesView.update(this.negociacoes);
+        this.mensagemView.update('Negociação adicionada com Sucesso');
         this.limparFormulario();
     }
     criaNegociacao() {
