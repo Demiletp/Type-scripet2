20c7e2066cb2d253781d9d2d2dcd7791cefd5c5e
Demiletp <141511721+Demiletp@users.noreply.github.com>
2024-06-13 16:20:57 -0300

    COMMIT 1

--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -10,4 +10,11 @@ export class Negociacoes {
     public lista(): readonly Negociacao[] {
         return this.negociacoes;
     }
+    public criaDe(dataString: string, quantidadeString: string, valorString: string): Negociacao{
+        const exp = /-/g;
+        const date = new Date(dataString.replace(exp, ','));
+        const quantidade = parseInt(quantidadeString);
+        const valor = parseFloat(valorString);
+        return new Negociacao(date, quantidade, valor);
+    }
 }
