47c72aebb22cd4b79f0bae53b3a4e723f10c95ff
Demiletp <141511721+Demiletp@users.noreply.github.com>
2024-06-13 16:36:06 -0300

    COMMIT 3

--- app/views/view.ts
+++ app/views/view.ts
@@ -2,7 +2,7 @@ export abstract class View <T> {
 
     protected elemento: HTMLElement;
 
-    constructor(seletor: string){
+    constructor(seletor: string, escapar: boolean){
         this.elemento = document.querySelector(seletor);
     }
      public update(model: T): void{
